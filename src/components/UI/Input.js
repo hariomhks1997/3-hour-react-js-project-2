d285c458db71bbc05428732f35ffb11cdca7cb58
Your Name01~ <you@example.com>
2023-07-18 10:14:11 +0530

    Adding a Form

---
+++ src/components/UI/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+const Input = (props) => {
+    console.log({...props.input})
+  return (
+    <div className={classes.input}>
+        <label htmlFor={props.input.id}>{props.label}</label>
+        <input {...props.input}></input>
+
+    </div>
+  )
+}
+
+export default Input
